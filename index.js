565b405a471956314a5b7f92bd99c55ee35064b7
edilbertomg <edilbertomarrugo03@hotmail.com>
2020-02-17 17:03:03 -0500

    first commit

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const path = require('path');
 const app = express();
 
 app.use(express.static(__dirname));
-app.get('/',function(req,res){
+app.get('/photos',function(req,res){
     res.sendFile(path.join(__dirname+'index.html'));
 });
 
